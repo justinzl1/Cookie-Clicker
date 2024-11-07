e4f725ae2827038e6db55e5069295d153ace4e19
jlee824 <justinzlee2016@gmail.com>
2024-11-06 17:34:20 -0700

    added functionality, player can only buy an ugprade if they have sufficient cookies

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-
-
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [cookies, setCookies] = useState(0)
-  const [cps, setCPS] = useState(0)
-  const [add, setAdd] = useState(1)
+  const [cookies, setCookies] = useState(0);
+  const [cps, setCPS] = useState(0);
+  const [add, setAdd] = useState(1);
   const [cost1, setCost1] = useState(10);
   const [cost2, setCost2] = useState(100);
 
   useEffect(() => {
-      // Set up an interval to execute a function every second
-      const interval = setInterval(() => {
-          setCookies(cookies => cookies + cps); // Update state every second
-      }, 1000); // 1000 milliseconds = 1 second
+    // Set up an interval to execute a function every second
+    const interval = setInterval(() => {
+      setCookies((cookies) => cookies + cps); // Update state every second
+    }, 1000); // 1000 milliseconds = 1 second
 
-      // Clean up the interval on component unmount
-      return () => clearInterval(interval);
+    // Clean up the interval on component unmount
+    return () => clearInterval(interval);
   });
   return (
     <>
       <h1>Cookie Clicker</h1>
       <div>
-        <p>{Number(cookies.toFixed(2))} Cookies<br/ >per second: {Number(cps.toFixed(2))}</p>
+        <p>
+          {Number(cookies.toFixed(2))} Cookies
+          <br />
+          per second: {Number(cps.toFixed(2))}
+        </p>
       </div>
       <div className="cookie">
         <button onClick={() => setCookies((cookies) => cookies + add)}>
@@ -31,15 +33,33 @@ function App() {
         </button>
       </div>
       <div className="button">
-        <button onClick={() => {setAdd((add) => add + 1), setCookies((cookies) => cookies - cost2), setCost2((cost2) => cost2 * 1.2)}}>
-          Upgrade +1 Click <br />Cost: {Number(cost2.toFixed(0))}
+        <button
+          onClick={() => {
+            if (cookies > cost2) {
+              setAdd((add) => add + 1);
+              setCookies((cookies) => cookies - cost2);
+              setCost2((cost2) => cost2 * 1.2);
+            }
+          }}
+        >
+          Upgrade +1 Click <br />
+          Cost: {Number(cost2.toFixed(0))}
         </button>
-        <button onClick={() => {setCPS((cps) => cps + 0.1), setCookies((cookies) => cookies - cost1), setCost1((cost1) => cost1 * 1.2)}}>
-          Upgrade +0.1 CPS <br />Cost: {Number(cost1.toFixed(0))}
+        <button
+          onClick={() => {
+            if (cookies > cost1) {
+              setCPS((cps) => cps + 0.1);
+              setCookies((cookies) => cookies - cost1);
+              setCost1((cost1) => cost1 * 1.2);
+            }
+          }}
+        >
+          Upgrade +0.1 CPS <br />
+          Cost: {Number(cost1.toFixed(0))}
         </button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
